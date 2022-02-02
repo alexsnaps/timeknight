df1d5dfee695776f357399bf67011900d545a2b2
Alex Snaps <alex@wcgw.dev>
2022-02-02 11:32:33 -0500

    Small refactorings

--- src/storage/fs.rs
+++ src/storage/fs.rs
@@ -1,18 +1,19 @@
 use std::fs::{remove_file, OpenOptions};
+use std::io;
 use std::io::ErrorKind;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 struct FsStorage {
-  location: String,
+  location: PathBuf,
 }
 
 impl FsStorage {
-  fn new(location: &Path) -> Result<Self, ErrorKind> {
+  pub fn new(location: &Path) -> Result<Self, ErrorKind> {
     if !location.is_dir() {
       return Err(ErrorKind::InvalidInput);
     }
 
-    let lock_location = location.join(".lock");
+    let lock_location = Self::lock_file(location);
 
     match OpenOptions::new()
       .write(true)
@@ -20,17 +21,26 @@ impl FsStorage {
       .open(lock_location)
     {
       Ok(_) => Ok(FsStorage {
-        location: location.to_str().unwrap().to_string(),
+        location: location.to_path_buf(),
       }),
       Err(err) => Err(err.kind()),
     }
+  }
+
+  fn lock_file(location: &Path) -> PathBuf {
+    location.join(".lock")
+  }
+
+  fn close(&mut self) -> Result<(), io::Error> {
+    remove_file(Self::lock_file(self.location.as_path()))
   }
 }
 
 impl Drop for FsStorage {
   fn drop(&mut self) {
-    remove_file(Path::new(self.location.as_str()).join(".lock"))
-      .expect("Failed to remove lock file!");
+    if self.close().is_err() {
+      eprintln!("Failed to remove lock file!")
+    }
   }
 }
 
@@ -53,10 +63,10 @@ mod tests {
 
   #[test]
   fn test_succeeds_on_proper_dir() {
-    let location = env::temp_dir().join("tracetimeTest");
+    let location = env::temp_dir().join("tracetimeTest_succeeds_on_proper_dir");
     create_dir(location.as_path()).expect("failed to create temp directory");
     {
-      let _existing_storate = FsStorage::new(location.as_path()).expect("Failed creating Storage");
+      let _working_storage = FsStorage::new(location.as_path()).expect("Failed creating Storage");
       assert_eq!(
         FsStorage::new(location.as_path()).err(),
         Some(ErrorKind::AlreadyExists)
